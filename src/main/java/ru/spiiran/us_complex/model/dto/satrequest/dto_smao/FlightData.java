a159a78427fdb13ecee332e25ad05cec47a5a006
kharisovilyas <91402961+kharisovilyas@users.noreply.github.com>
2024-05-14 20:45:29 +0300

    Разработка связи смао-про42-арм исправление неточности-14

--- src/main/java/ru/spiiran/us_complex/model/dto/satrequest/dto_smao/FlightData.java
+++ src/main/java/ru/spiiran/us_complex/model/dto/satrequest/dto_smao/FlightData.java
@@ -8,38 +8,5 @@ public class FlightData {
     private List<Long> sunTime;
     private List<Long> eclTime;
 
-    public FlightData() {
-    }
-
-    public int getnRev() {
-        return nRev;
-    }
-
-    public void setnRev(int nRev) {
-        this.nRev = nRev;
-    }
-
-    public List<Long> getRevTime() {
-        return revTime;
-    }
-
-    public void setRevTime(List<Long> revTime) {
-        this.revTime = revTime;
-    }
-
-    public List<Long> getSunTime() {
-        return sunTime;
-    }
-
-    public void setSunTime(List<Long> sunTime) {
-        this.sunTime = sunTime;
-    }
-
-    public List<Long> getEclTime() {
-        return eclTime;
-    }
-
-    public void setEclTime(List<Long> eclTime) {
-        this.eclTime = eclTime;
-    }
+    public FlightData() {}
 }
